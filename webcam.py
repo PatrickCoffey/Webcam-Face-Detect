124a5504aaefbf690b37cb69b34d47cb100953e2
PatrickCoffey <patrick.coffey@nt.gov.au>
2014-10-14 09:16:30 +0930

    inc. frame skip
    include frame skip to imporve performance

--- webcam.py
+++ webcam.py
@@ -1,32 +1,53 @@
+# Taken from Shatnu (forked repo on github)
+
 import cv2
 import sys
 
-cascPath = sys.argv[1]
+#cascPath = sys.argv[1]
+cascPath = "./haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 
+counter = 1
+counterMax = 4
+
+counterMax = counter + 1
+
+global face
+face = 0
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
+    print counter
+    if counter == 1:
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        global faces
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )    
+    
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
-
+    
+    # Increment counter
+    counter += 1
+    
+    #check if we need to reset counter
+    if counter == counterMax:
+        counter = 1
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
